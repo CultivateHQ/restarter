2ed8ebbdc5bd8fd74865165f6e7cfeeadf16dd2d
Paul Wilson <paulanthonywilson@gmail.com>
2016-07-25 15:33:26 +0100

    Ready to publish

--- mix.exs
+++ mix.exs
@@ -5,31 +5,37 @@ defmodule Restarter.Mixfile do
     [app: :restarter,
      version: "0.1.0",
      elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+     description: description(),
+     package: package(),
+     deps: deps(),]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:credo, "~> 0.4", only: [:dev, :test]},
       {:ex_doc, "~> 0.12", only: :dev},
+    ]
+  end
+
+  defp description do
+  """
+  The `Restarter` allows you to set a retry interval for restarting a child process. The child will be restarted for indefinitely.
+
+  Useful for processes that rely on external connections
+  """
+  end
+
+  defp package do
+    [
+      name: :restarter,
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Paul Wilson"],
+      licenses: ["MIT"],
+      links: %{}
     ]
   end
 end
